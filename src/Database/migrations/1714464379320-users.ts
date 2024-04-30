6958896c5c0a44022d4c315d672af5717822794e
Fernando <fernandocatalamunyoz@gmail.com>
2024-04-30 10:21:22 +0200

    feat: creacion migracion users

--- src/Database/migrations/1714464379320-users.ts
+++ src/Database/migrations/1714464379320-users.ts
@@ -1,11 +1,80 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Users1714464379320 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "firstName",
+            type: "varchar",
+            length: "255",
+          },
+          {
+            name: "lastName",
+            type: "varchar",
+            length: "255",
+          },
+          {
+            name: "country",
+            type: "varchar",
+            length: "40",
+          },
+          {
+            name: "email",
+            type: "varchar",
+            length: "255",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+            length: "255",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+            onUpdate: "now()",
+          },
+          {
+            name: "is_active",
+            type: "boolean",
+            default: true,
+          },
+          {
+            name: "role_id",
+            type: "int",
+            isNullable: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ["role_id"],
+            referencedTableName: "roles",
+            referencedColumnNames: ["id"],
+            onDelete: "CASCADE",
+          },
+        ],
+      }),
+      true
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
 }
