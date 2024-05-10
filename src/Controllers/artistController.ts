5fed4b470ad7cdf97982e2d0d9af0723ac0a56f6
Fernando <fernandocatalamunyoz@gmail.com>
2024-05-10 17:35:08 +0200

    feat: arreglo paginacion Artists

--- src/Controllers/artistController.ts
+++ src/Controllers/artistController.ts
@@ -106,16 +106,7 @@ export const deleteArtist = async (req: Request, res: Response) => {
 //TRAER TODOS LOS ARTISTAS
 export const getArtists = async (req: Request, res: Response) => {
   try {
-    let limit = Number(req.query.limit) || 9;
-    const page = Number(req.query.page) || 1;
-    const skip = (page - 1) * limit;
-
-    if (limit > 100) {
-      limit = 10;
-    }
     const artists = await Artist.find({
-      take: limit,
-      skip: skip,
       relations: { genre: true },
     });
     res.status(200).json({
